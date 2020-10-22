6fd38b4e3dcab3e4d46878c123cd085f354c44fa
koshachu <koshachu@gmail.com>
2020-10-22 17:13:26 +1100

    setting upper boundary for the projectile

--- Gnome Slope/Assets/Scripts/ProjectileMovement.cs
+++ Gnome Slope/Assets/Scripts/ProjectileMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ProjectileMovement : MonoBehaviour
 {
     public float projectileSpeed;
+    private float topBoundaryZ = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,11 @@ public class ProjectileMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.z > topBoundaryZ)
+        {
+            Destroy(gameObject);
+        }
         transform.Translate(Vector3.forward * Time.deltaTime * projectileSpeed);
+
     }
 }
