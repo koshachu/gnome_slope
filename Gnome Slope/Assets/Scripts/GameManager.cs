a2d949acc641d17b394c188b0d76232fef2bd9ea
koshachu <koshachu@gmail.com>
2020-10-28 18:33:56 +1100

    adding GameManager
    disabling title screen on game start enabling snow effect on game start

---
+++ Gnome Slope/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public ParticleSystem snow;
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        snow.Play();
+        titleScreen.gameObject.SetActive(false);
+    }
+}
