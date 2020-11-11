ae58d2cd124026740d20af4f725bcb64c262168e
koshachu <koshachu@gmail.com>
2020-11-11 18:37:33 +1100

    amend changes

---
+++ Gnome Slope/Assets/Scripts/PowerupCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Speed boost");
+        Destroy(gameObject);
+    }
+
+
+}
